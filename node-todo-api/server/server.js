01c5c7299e3204489d048c511c6e9c43aca935d6
Kshitij Pratap Singh Bhadauria <kshitijpratap0809@gmail.com>
2018-03-28 05:05:22 +0530

    validations in model schema

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -6,39 +6,69 @@ mongoose.connect('mongodb://localhost:27017/TodoApp');
 
 
 
-var todo=mongoose.model('todo',{
-text:	{
-		type:String
-	},
-completed:	{
-		type:Boolean
-	},
-completedAt:	{
-		type:Number
-	}
-});
-
-
-
-// var newtodo=new todo({
-// 	text:"iitr"
+// var todo=mongoose.model('todo',{
+// text:	{
+// 		type:String,
+// 		required:true,
+// 		minlength:1,
+// 		trim:true
+// 	},
+// completed:	{
+// 		type:Boolean,
+// 		default:true
+// 	},
+// completedAt:	{
+// 		type:Number,
+// 		default:100
+// 	}
 // });
 
-// newtodo.save().then((docs)=>{
-//   console.log("saved doc",docs);
+
+
+// // var newtodo=new todo({
+// // 	text:"iitr"
+// // });
+
+// // newtodo.save().then((docs)=>{
+// //   console.log("saved doc",docs);
+// // },(e)=>{
+// // 	console.log(e);
+// // });
+
+
+// var otherTodo=new todo({
+// 	   text:true,
+// 	// completed:false,
+// 	// completedAt:120
+// });
+
+// otherTodo.save().then((docs)=>{
+//   console.log(JSON.stringify(docs,undefined,2));
 // },(e)=>{
 // 	console.log(e);
 // });
 
 
-var otherTodo=new todo({
-	text:"iitr",
-	completed:false,
-	completedAt:120
+
+var User=mongoose.model('User',{
+email:	{
+		type:String,
+		required:true,
+		minlength:1,
+		trim:true
+	}
 });
 
-otherTodo.save().then((docs)=>{
-  console.log(JSON.stringify(docs,undefined,2));
+
+
+
+
+var user=new User({
+	   email:"kshitijpratap0809@gmail.com"
+});
+
+user.save().then((docs)=>{
+  console.log("saved doc",docs);
 },(e)=>{
 	console.log(e);
 });
