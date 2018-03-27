152437405e5ef96a9e62cf13adf31fdcdc23ee43
Kshitij Pratap Singh Bhadauria <kshitijpratap0809@gmail.com>
2018-03-27 08:24:23 +0530

    A FILTER FUNCTION

--- node-todo-api/playground/mongodb-find.js
+++ node-todo-api/playground/mongodb-find.js
@@ -10,12 +10,27 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp",(err,db)=>{
   console.log("connected to the server");
 
 
-  db.collection('Todos').find({
-  _id:new ObjectID('5ab9a5f34c6bb9b16108f4f0')
-  }).toArray().then((docs)=>{
-     console.log('Todos');
-     console.log(JSON.stringify(docs,undefined,3));
-  },(err)=>{
+  // db.collection('Todos').find({
+  // _id:new ObjectID('5ab9a5f34c6bb9b16108f4f0')
+  // }).toArray().then((docs)=>{
+  //    console.log('Todos');
+  //    console.log(JSON.stringify(docs,undefined,3));
+  // },(err)=>{
+  // 	console.log("unable to find the document");
+  // });
+
+
+
+  // db.collection('Todos').find().count().then((count)=>{
+  //    console.log(`Todos-count:${count}`);
+  // },(err)=>{
+  // 	console.log("unable to find the document");
+  // });
+ 
+
+   db.collection('Users').find({name:'kshitij'}).toArray().then((docs)=>{
+		console.log(JSON.stringify(docs,undefined,3)); 
+ },(err)=>{
   	console.log("unable to find the document");
   });
 
