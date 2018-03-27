7407a9b7357b68b37f5da7e8f7fadac79f36e4be
Kshitij Pratap Singh Bhadauria <kshitijpratap0809@gmail.com>
2018-03-28 01:41:56 +0530

    update functionality

---
+++ node-todo-api/playground/mongodb-update.js
@@ -0,0 +1,33 @@
+//const MongoClient=require('mongodb').MongoClient;
+
+const {MongoClient,ObjectID}=require('mongodb');
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp",(err,db)=>{
+  
+  if(err){
+  	console.log("unable to connect to server");
+  }
+  console.log("connected to the server");
+
+
+   // db.collection('Users').deleteMany({name:'kshitij'}).then((result)=>{
+   //  console.log(result);
+   // });
+      
+
+
+      db.collection('Users').findOneAndUpdate({name:"kshitij"},
+        {
+          $set:{
+            name:"pawan"
+          }
+        },{
+            returnOriginal:false
+          }
+        ).then((result)=>{
+    console.log(result);
+   });
+
+  //db.close();
+ });
+
