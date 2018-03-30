66aacc8ac6105e1b5669625556be9e510ba8dd76
Kshitij Pratap Singh Bhadauria <kshitijpratap0809@gmail.com>
2018-03-30 07:39:08 +0530

    added  mongoose queries in playground folder

--- node-todo-api/playground/mongoose-queries.js
+++ node-todo-api/playground/mongoose-queries.js
@@ -2,33 +2,45 @@ const {mongoose}=require("./../server/db/mongoose.js");
 const {ObjectId}=require('mongodb');
 
 const {Todo} =require("./../server/models/todo.js");
+const {User} =require("./../server/models/user.js");
 
 
-var id='abd93a781cf61c02463bcf9';
+// var id='5abd93a781cf61c02463bcf9';
 
 
-if(!ObjectId.isValid(id)){
-	console.log('id not found');
-}
+// if(!ObjectId.isValid(id)){
+// 	console.log('id not found');
+// }
 
-Todo.find({
-	_id:id
-}).then((todo)=>{
-	console.log('Todo',todo);
-});
+// Todo.find({
+// 	_id:id
+// }).then((todo)=>{
+// 	console.log('Todo',todo);
+// });
 
 
-Todo.findOne({
-	_id:id
-}).then((todo)=>{
-	console.log('Todo',todo);
-});
+// Todo.findOne({
+// 	_id:id
+// }).then((todo)=>{
+// 	console.log('Todo',todo);
+// });
 
-Todo.findById(id).then((todo)=>{
+// Todo.findById(id).then((todo)=>{
+// 	if(!todo){
+// 		return console.log('id not found');
+// 	}
+// 	console.log('Todo by id',todo);
+// }).catch((e)=>{
+// 	console.log(e);
+// });
+
+var id='5abad4234dc1afac203cdbbe';
+
+User.findById(id).then((todo)=>{
 	if(!todo){
 		return console.log('id not found');
 	}
-	console.log('Todo by id',todo);
+	console.log(JSON.stringify(todo,undefined,2));
 }).catch((e)=>{
 	console.log(e);
 });
