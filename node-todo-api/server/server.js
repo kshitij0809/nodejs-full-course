c57085b337f0ab5efc107e9651e0821acbb8fe00
Kshitij Pratap Singh Bhadauria <kshitijpratap0809@gmail.com>
2018-03-30 08:09:57 +0530

    GET /todos/:id method

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -1,6 +1,7 @@
 var express=require("express");
 var bodyParser=require("body-parser");
 
+const {ObjectId}=require('mongodb');
 
 var {mongoose}=require("./db/mongoose.js");
 
@@ -14,14 +15,17 @@ var app=express();
  
 app.use(bodyParser.json());
 
+
+
+///POST method
+
+
+
 app.post('/todos',(req,res)=>{
 	
     var todo=new Todo({
     	text:req.body.text
     });
-
-
-
 
   todo.save().then((docs)=>{
      res.send(docs);
@@ -33,8 +37,12 @@ app.post('/todos',(req,res)=>{
 });
 
 
+
+
+///GET method
+
+
 app.get('/todos',(req,res)=>{
-	
    
 
 	Todo.find().then((todos)=>{
@@ -42,6 +50,35 @@ app.get('/todos',(req,res)=>{
 	},(e)=>{
 		res.status(400).send(e);
 	});
+
+
+});
+
+
+
+
+///GET method by ID
+
+
+app.get('/todos/:id',(req,res)=>{
+   
+
+   var id=req.params.id;
+    
+    if(!ObjectId.isValid(id)){
+	return res.status(404).send();
+}
+
+
+	Todo.findById(id).then((todo)=>{
+		if(!todo){
+			return res.status(404).send();
+		}
+	     res.send({todo});
+		}).catch((e)=>{
+			res.status(404).send();
+		});
+	
 
 
 });
