a4fb9cd540cc3ee95d36f3bd3ed47310bbf3813d
Kshitiz Pratap <kshitijpratap0809@gmail.com>
2018-05-11 06:23:06 -0400

    basic routing

---
+++ exp-myapp/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+app.get('/app', function (req, res) {
+  res.send('Hello do World!')
+})
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
+
+
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user')
+})
+
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user')
+})
+
+app.use('/static', express.static('public'))
