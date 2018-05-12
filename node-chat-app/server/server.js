7a656a2898698f60ecc3e26bde00173d12690b3c
Kshitiz Pratap <kshitijpratap0809@gmail.com>
2018-05-12 05:37:51 -0400

    add connect and disconnect event handelers

--- node-chat-app/server/server.js
+++ node-chat-app/server/server.js
@@ -1,12 +1,27 @@
 const path = require('path');
 const express =require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+
+
 const publicPath=path.join(__dirname,'../public');
 
 var app = express();
+var server=http.createServer(app);
+var io=socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(3000,()=>{
+io.on('connection',(socket)=>{
+	console.log('new user connected');
+
+	socket.on('disconnect',()=>{
+    console.log("user disconnected to server");
+  });
+});
+
+
+server.listen(3000,()=>{
 	console.log("server started at port 3000");
 })
 
